ae072095cd60d941ff5085d68faf4d774677e6f0
dcifligu <deivi.cifligu@gmail.com>
2024-09-07 20:31:32 +0200

    Windows commit

--- src/components/Consultation.vue
+++ src/components/Consultation.vue
@@ -2,7 +2,19 @@
 
 <template>
   <div class="flex justify-center relative z-0">
-    <div class="w-5/6 h-screen m-6 bg-custom-gradient rounded-3xl"></div>
+    <div class="w-5/6 h-screen m-6 bg-custom-gradient rounded-3xl flex justify-between">
+      <div class="flex flex-col">
+        <span>
+          Book your free consultation
+        </span>
+        <span>
+          with our experts
+        </span>
+      </div>
+      <button class="z-10 text-[#212121] text-xl font-semibold mx-4 h-fit">
+        Claim Your Free Consultation
+      </button>
+    </div>
     <div
       class="absolute z-0 w-[150%] h-full bg-[url('src/assets/images/honyecomb.svg')] bg-cover bg-center bg-repeat mix-blend-overlay"
     ></div>
