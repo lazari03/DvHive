1eb532b75a49c867be8070d248f5cd3a78f6b8b9
dcifligu <deivi.cifligu@gmail.com>
2024-09-16 16:46:05 +0200

    Add text_middle component to IntakeForm.vue

--- src/IntakeForm.vue
+++ src/IntakeForm.vue
@@ -4,12 +4,17 @@ import { defineAsyncComponent } from 'vue'
 const Hero_Intake = defineAsyncComponent(() => import('./components/Hero_Intake.vue'))
 const License_banner = defineAsyncComponent(() => import('./components/license_banner.vue'))
 const FooterComponent = defineAsyncComponent(() => import('./components/FooterComponent.vue'))
+const text_middle = defineAsyncComponent(() => import('./components/text_middle.vue'))
 </script>
 
 <template>
   <div class="bg-[#212121] font-montserrat font-medium relative">
-    <Hero_Intake header="Intake Form" subheader="Subheader" />
+    <Hero_Intake header="We Help You Recover Your Vehicle's Value" subheader="Contact Us Today!" />
     <License_banner />
+    <text_middle
+      title="Explore the Appraisal Service Tailored to Your Needs. From Free Quotes to In-Depth Evaluations, We’ve Got You Covered."
+      subtitle=" "
+    />
     <FooterComponent />
   </div>
 </template>
---
+++ src/components/Intake_form.vue
@@ -0,0 +1,63 @@
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import 'intl-tel-input/build/css/intlTelInput.css'
+import intlTelInput from 'intl-tel-input'
+
+const iti = ref(null)
+
+onMounted(() => {
+  const input = document.querySelector('#phone')
+  iti.value = intlTelInput(input, {
+    utilsScript: '/node_modules/intl-tel-input/build/js/utils.js',
+    containerClass: 'w-full',
+    initialCountry: 'us'
+  })
+})
+
+const formData = ref({
+  full_name: '',
+  email: '',
+  phone: '',
+  claimType: '',
+  vehicleYear: '',
+  vehicleMake: '',
+  vehicleModel: '',
+  vehicleTrim: '',
+  vehicleMileage: '',
+  accidentDate: '',
+  state: '',
+  estimatedRepair: '',
+  priorAccidents: '',
+  prevEstimatedRepair: '',
+  leasing: '',
+  insurance: '',
+  faultInsurance: '',
+  message: '',
+  file: ''
+})
+
+async function handleSubmitIntake() {
+  formData.value.phone = iti.value.getNumber()
+
+  if (!iti.value.isValidNumber()) {
+    alert('Invalid phone number.')
+    return
+  }
+
+  try {
+    const response = await axios.post('http://localhost:3000/intake-email', formData.value)
+    console.log('Email sent successfully:', response.data)
+    alert('Email sent successfully!')
+  } catch (error) {
+    console.error('Failed to send email:', error)
+    alert('Failed to send email.')
+  }
+}
+</script>
+
+<template>
+  <div class="h-fit bg-[#F0ECE5] w-2/6 rounded-xl shadow-2xl py-6">
+    <form @submit.prevent="handleSubmitIntake" class="p-6" action=""></form>
+  </div>
+</template>
