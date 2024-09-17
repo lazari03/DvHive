32881ff89d0c730b5e34bf130f6492cc6384ae65
dcifligu <deivi.cifligu@gmail.com>
2024-09-17 14:33:50 +0200

    Update email API server.js

--- email-api/server.js
+++ email-api/server.js
@@ -25,14 +25,14 @@ app.post('/consultation-email', async (req, res) => {
   const { full_name, email, phone, message } = req.body
 
   const htmlContent = `
-  <img src="https://github.com/dcifligu/consultation_images/blob/b2a273a96d9a208b6992e5ee7a0432943c5e9879/ConsulatitonFormEmail.png?raw=true" style="width: 700px; height: 300px; display: block; margin: auto;" />
+  <img src="https://github.com/dcifligu/consultation_images/blob/main/ConsultationFormEmail.png?raw=true" style="width: 700px; height: 300px; display: block; margin: auto;" />
   <table style="width: 700px; margin: auto; height: fit; background-color: white;">
     <tr>
       <td>
-        <h1 style="font-size: 20px; font-weight: 600; font-family: Arial, sans-serif;">Name: <span style="font-size: 16px; font-family: Arial, sans-serif;">${full_name}</span></h1>
-        <h1 style="font-size: 20px; font-weight: 600; font-family: Arial, sans-serif;">E-mail: <span style="font-size: 16px; font-family: Arial, sans-serif;">${email}</span></h1>
-        <h1 style="font-size: 20px; font-weight: 600; font-family: Arial, sans-serif;">Phone: <span style="font-size: 16px; font-family: Arial, sans-serif;">${phone}</span></h1>
-        <h1 style="font-size: 20px; font-weight: 600; font-family: Arial, sans-serif;">Message: <span style="font-size: 16px; font-family: Arial, sans-serif;">${message}</span></h1>
+        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Name: <span style="font-size: 16px; font-family: Arial, sans-serif;">${full_name}</span></h1>
+        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">E-mail: <span style="font-size: 16px; font-family: Arial, sans-serif;">${email}</span></h1>
+        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Phone: <span style="font-size: 16px; font-family: Arial, sans-serif;">${phone}</span></h1>
+        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Message: <span style="font-size: 16px; font-family: Arial, sans-serif;">${message}</span></h1>
       </td>
     </tr>
   </table>
@@ -42,6 +42,70 @@ app.post('/consultation-email', async (req, res) => {
     from: email,
     to: process.env.RECIPIENT_EMAIL, // Recipient Mail
     subject: 'New Consultation Request',
+    priority: 'high',
+    html: htmlContent
+  }
+
+  try {
+    await transporter.sendMail(mailOptions)
+    res.status(200).send({ message: 'Email sent successfully' })
+  } catch (error) {
+    console.error('Error sending email:', error)
+    res.status(500).send({ message: 'Failed to send email' })
+  }
+})
+
+app.post('/intake-email', async (req, res) => {
+  const {
+    full_name,
+    email,
+    phone,
+    claimType,
+    vehicleYear,
+    vehicleMake,
+    vehicleModel,
+    vehicleTrim,
+    vehicleMileage,
+    accidentDate,
+    state,
+    estimatedRepair,
+    priorAccidents,
+    prevEstimatedRepair,
+    leasing,
+    insurance,
+    faultInsurance,
+    message
+  } = req.body
+
+  const htmlContent = `
+  <img src="https://github.com/dcifligu/consultation_images/blob/main/IntakeFormEmail.png?raw=true" style="width: 700px; height: 300px; display: block; margin: auto;" />
+  <table style="width: 700px; margin: auto; height: fit; color: #212121; padding: 5px;">
+    <tr>
+      <td>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Full Name:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${full_name}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Email:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${email}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Phone:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${phone}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Claim Type:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${claimType}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Vehicle Details:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${vehicleYear} ${vehicleMake} ${vehicleModel} ${vehicleTrim}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Mileage:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${vehicleMileage}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Accident Date:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${accidentDate}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>State:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${state}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Estimated Repair:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">$${estimatedRepair}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Prior Accidents:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${priorAccidents}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Previous Estimated Repair:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">$${prevEstimatedRepair}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Leasing:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${leasing}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Insurance:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${insurance}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Fault Insurance:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${faultInsurance}</span></h1>
+      <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;"><strong>Message:</strong> <span style="font-size: 16px; font-family: Arial, sans-serif;">${message}</span></h1>
+      </td>
+    </tr>
+  </table>
+  `
+
+  const mailOptions = {
+    from: email,
+    to: process.env.RECIPIENT_EMAIL, // Recipient Mail
+    subject: 'New Intake form submission',
     priority: 'high',
     html: htmlContent
   }
