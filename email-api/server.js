7ffaba42a2e301f727f8919153c7af6cee7f44e7
lazari03 <alfonzlazri@yea.al>
2025-11-27 01:19:47 +0100

    /

--- email-api/server.js
+++ email-api/server.js
@@ -36,47 +36,33 @@ const upload = multer({ dest: 'uploads/' })
 app.use(bodyParser.json())
 app.use(cors())
 
-// Nodemailer configuration for Gmail
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL, // Sender Email
-    pass: process.env.PASSWORD // Sender App Password
-  }
-})
+
 
 // Endpoint to handle form submission
 app.post('/consultation-email', async (req, res) => {
-  const { full_name, email, phone, message } = req.body
-
+  const { full_name, email, phone, message } = req.body;
   const htmlContent = `
-  <img src="https://github.com/dcifligu/consultation_images/blob/main/ConsultationFormEmail.png?raw=true" style="width: 700px; height: 300px; display: block; margin: auto;" />
-  <table style="width: 700px; margin: auto; height: fit; background-color: white;">
-    <tr>
-      <td>
-        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Name: <span style="font-size: 16px; font-family: Arial, sans-serif;">${full_name}</span></h1>
-        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">E-mail: <span style="font-size: 16px; font-family: Arial, sans-serif;">${email}</span></h1>
-        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Phone: <span style="font-size: 16px; font-family: Arial, sans-serif;">${phone}</span></h1>
-        <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Message: <span style="font-size: 16px; font-family: Arial, sans-serif;">${message}</span></h1>
-      </td>
-    </tr>
-  </table>
-  `
-
-  const mailOptions = {
-    from: email,
-    to: process.env.RECIPIENT_EMAIL, // Recipient Mail
-    subject: 'New Consultation Request',
-    priority: 'high',
-    html: htmlContent
-  }
-
+    <img src="https://github.com/dcifligu/consultation_images/blob/main/ConsultationFormEmail.png?raw=true" style="width: 700px; height: 300px; display: block; margin: auto;" />
+    <table style="width: 700px; margin: auto; height: fit; background-color: white;">
+      <tr>
+        <td>
+          <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Name: <span style="font-size: 16px; font-family: Arial, sans-serif;">${full_name}</span></h1>
+          <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">E-mail: <span style="font-size: 16px; font-family: Arial, sans-serif;">${email}</span></h1>
+          <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Phone: <span style="font-size: 16px; font-family: Arial, sans-serif;">${phone}</span></h1>
+          <h1 style="font-size: 18px; font-weight: 500; font-family: Arial, sans-serif;">Message: <span style="font-size: 16px; font-family: Arial, sans-serif;">${message}</span></h1>
+        </td>
+      </tr>
+    </table>
+  `;
   try {
-    await transporter.sendMail(mailOptions)
-    res.status(200).send({ message: 'Email sent successfully' })
+    await sendEmailWithBrevo({
+      subject: 'New Consultation Request',
+      htmlContent
+    });
+    res.status(200).send({ message: 'Email sent successfully' });
   } catch (error) {
-    console.error('Error sending email:', error)
-    res.status(500).send({ message: 'Failed to send email' })
+    console.error('Error sending email:', error);
+    res.status(500).send({ message: 'Failed to send email' });
   }
 })
 
@@ -118,20 +104,15 @@ app.post('/intake-email', upload.array('file'), async (req, res) => {
   </table>
   `
 
-  const mailOptions = {
-    from: process.env.EMAIL,
-    to: process.env.RECIPIENT_EMAIL,
-    subject: `New Intake Form: ${formData.full_name}`,
-    html: htmlContent,
-    attachments: attachments
-  }
-
   try {
-    await transporter.sendMail(mailOptions)
-    res.status(200).send({ message: 'Email sent successfully!' })
+    await sendEmailWithBrevo({
+      subject: `New Intake Form: ${formData.full_name}`,
+      htmlContent
+    });
+    res.status(200).send({ message: 'Email sent successfully!' });
   } catch (error) {
-    console.error('Error sending email:', error)
-    res.status(500).send({ message: 'Failed to send email.' })
+    console.error('Error sending email:', error);
+    res.status(500).send({ message: 'Failed to send email.' });
   }
 })
 
